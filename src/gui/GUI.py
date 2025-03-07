70e207f936dc105945481b75f1fb8cb2e1694ca4
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2025-03-07 11:03:01 +0100

    export segmented files works

--- src/gui/GUI.py
+++ src/gui/GUI.py
@@ -4,6 +4,7 @@ from gui.MainWindow import Ui_MainWindow
 from controller.Controller import Controller
 from shared.Commands import Command
 from PIL import ImageQt
+import os
 class GUI(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
@@ -11,10 +12,11 @@ class GUI(QMainWindow, Ui_MainWindow):
         self.setupUi(self.MainWindow)
         self.controller = Controller()
         self.image_path = None
-
+        self.segmented_image = None
         self.action_train_model.triggered.connect(self.onTrainModelClicked)
         self.action_open_image.triggered.connect(self.onOpenImageClicked)
         self.actionRun_Segmentation_on_Current_Image.triggered.connect(self.onSegmentImageClicked)
+        self.actionExport_Segmentation_2.triggered.connect(self.on_export_segmented_clicked)
 
     def onOpenImageClicked(self):
         file_path, _ = QFileDialog.getOpenFileName(self, "Select a file", "", "All Files (*)")
@@ -26,9 +28,8 @@ class GUI(QMainWindow, Ui_MainWindow):
     def onSegmentImageClicked(self):
         if (self.image_path == None):
             raise FileNotFoundError("You must open a file first.")
-        
-        segmented_image = self.controller.process_command(Command.SEGMENT,self.image_path)
-        segmented_image_temp = ImageQt.ImageQt(segmented_image)
+        self.segmented_image = self.controller.process_command(Command.SEGMENT,self.image_path)
+        segmented_image_temp = ImageQt.ImageQt(self.segmented_image)
         pixmap = QPixmap.fromImage(segmented_image_temp)
         self.plot3.setPixmap(pixmap.scaled(600, 600, aspectRatioMode=1))
 
@@ -36,4 +37,25 @@ class GUI(QMainWindow, Ui_MainWindow):
         self.controller.process_command(Command.RETRAIN, "data/images", "data/masks")
 
 
+    def on_export_segmented_clicked(self):
+        print("clicked export")
+        
+        if(self.segmented_image == None):
+            raise FileNotFoundError("Error: Found no segmented image")
+
+        file_path, selected_filter = QFileDialog.getSaveFileName(None, "Save Image", "", "PNG Files (*.png);;JPEG Files (*.jpg);;All Files (*)")
+
+        if file_path: 
+            # Extract extension from selected filter if not provided
+            if not os.path.splitext(file_path)[1]:  # If no extension
+                if "PNG" in selected_filter:
+                    file_path += ".png"
+                elif "JPEG" in selected_filter:
+                    file_path += ".jpg"
+                else:
+                    file_path += ".png"  # Default to PNG
+            
+            self.segmented_image.save(file_path)
+            print(f"Image saved successfully to {file_path}")
+        
 
