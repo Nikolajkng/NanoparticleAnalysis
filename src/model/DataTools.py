1b909b037e27f3f9ebe3b575d925996cd4204d19
Christian <cvedelpetersen@gmail.com>
2025-03-12 15:13:44 +0100

    Changed resize_images to support non_masks

--- src/model/DataTools.py
+++ src/model/DataTools.py
@@ -1,5 +1,7 @@
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader, random_split
+import os
+from PIL import Image
 
 def get_dataloaders(dataset: Dataset, train_data_size: float) -> tuple[DataLoader, DataLoader]:
     
@@ -8,3 +10,19 @@ def get_dataloaders(dataset: Dataset, train_data_size: float) -> tuple[DataLoade
     val_dataloader = DataLoader(val_data, batch_size=1, shuffle=True)
 
     return (train_dataloader, val_dataloader)
+
+def resize_images(folder_path, output_size=(256, 256), is_masks=False):
+    for filename in os.listdir(folder_path):
+        if filename.endswith(('.tif')):
+            image_path = os.path.join(folder_path, filename)
+            img = Image.open(image_path)
+            img = img.convert("L")  
+            img_resized = img.resize(output_size, Image.NEAREST)
+            if is_masks:
+                img_resized = img_resized.point(lambda p: 255 if p > 20 else 0)
+            img_resized.save(image_path)  # You can change this line to save it elsewhere
+            print(image_path)
+
+if __name__ == '__main__':
+    folder_path = 'data/masks'
+    resize_images(folder_path, is_masks=True)
