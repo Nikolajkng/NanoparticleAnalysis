7f706a234afb2a1d2e2945581e659b0c47e61dbb
Christian <cvedelpetersen@gmail.com>
2025-03-19 11:05:35 +0100

    Fixed error with resizing images (locked files?)

--- src/model/DataTools.py
+++ src/model/DataTools.py
@@ -6,9 +6,9 @@ from PIL import Image
 def get_dataloaders(dataset: Dataset, train_data_size: float) -> tuple[DataLoader, DataLoader]:
     
     train_data, val_data = random_split(dataset, [train_data_size, 1-train_data_size])
-    train_dataloader = DataLoader(train_data, batch_size=4, shuffle=True)
-    val_dataloader = DataLoader(val_data, batch_size=1, shuffle=True)
-
+    train_dataloader = DataLoader(train_data, batch_size=12, shuffle=True, drop_last=True)
+    val_dataloader = DataLoader(val_data, batch_size=3, shuffle=True, drop_last=True)
+    
     return (train_dataloader, val_dataloader)
 
 def resize_and_save_images(folder_path, output_size=(256, 256), is_masks=False):
@@ -19,10 +19,12 @@ def resize_and_save_images(folder_path, output_size=(256, 256), is_masks=False):
             img = img.convert("L")  
             img_resized = img.resize(output_size, Image.NEAREST)
             if is_masks:
-                img_resized = img_resized.point(lambda p: 255 if p > 20 else 0)
-            img_resized.save(image_path)  # You can change this line to save it elsewhere
+                img_binary = img_resized.point(lambda p: 255 if p > 20 else 0)
+                img_binary.save(os.path.join(folder_path,"new"+filename))
+            else:    
+                img_resized.save(image_path)  # You can change this line to save it elsewhere
             print(image_path)
 
 if __name__ == '__main__':
-    folder_path = 'data/masks'
-    resize_and_save_images(folder_path, is_masks=True)
+    folder_path = 'data/images/'
+    resize_and_save_images(folder_path, is_masks=False)
