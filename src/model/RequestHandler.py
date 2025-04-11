1ae09074cd7d9a6fbd8ef14d8887e78e790f0c2f
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2025-04-11 11:37:03 +0200

    renamed to write_stat_to_txt

--- src/model/RequestHandler.py
+++ src/model/RequestHandler.py
@@ -42,7 +42,7 @@ class request_handler:
         
         num_labels, labels, stats, centroids = analyzer.get_connected_components(segmented_image_2d)
         table_data = analyzer.format_table_data(stats, scale_info, num_labels)
-        analyzer.write_statistics_to_txt(stats, scale_info, num_labels)
+        analyzer.write_stats_to_txt(stats, scale_info, num_labels)
         annotated_image = analyzer.add_annotations(segmented_image_2d, centroids)
         image_pil = Image.fromarray(annotated_image)
         
