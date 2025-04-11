9d4ac1c5261b9b6e78a8eb07686d8460bb2d988b
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2025-04-11 10:43:08 +0200

    added number label on particles

--- src/model/SegmentationAnalyzer.py
+++ src/model/SegmentationAnalyzer.py
@@ -9,12 +9,25 @@ class SegmentationAnalyzer():
         return num_labels, labels, area_stats, centroids
     
     def add_annotations(self, image, centroids):
-        image_rgb = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
-        for i in range(1, len(centroids)):
-            x, y = int(centroids[i][0]), int(centroids[i][1])
+        try:
+            image_rgb = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+            font = cv2.FONT_HERSHEY_SIMPLEX 
+            font_scale = 0.3
+            thickness = 1
 
-            cv2.circle(image_rgb, (x, y), radius=1, color=(0, 0, 255), thickness=-1)
-        return image_rgb
+            for i in range(1, len(centroids)):
+                cX, cY = int(centroids[i][0]), int(centroids[i][1])
+                label = str(i)
+                (text_width, text_height), _ = cv2.getTextSize(label, font, font_scale, thickness)
+                text_x = cX - text_width // 2
+                text_y = cY + text_height // 2  
+                cv2.putText(image_rgb, label, (text_x, text_y), font, font_scale, (255, 0, 0), thickness, lineType=cv2.LINE_AA)
+                
+            return image_rgb
+        except Exception as e:
+            print("Error in add_annotations: ", e)
+            return image
+
     
     def __get_diameters(self, stats: np.ndarray):
         diameters = np.empty(stats.shape[0]-1)
@@ -28,7 +41,7 @@ class SegmentationAnalyzer():
         return stats[1:, cv2.CC_STAT_AREA] 
     
 
-    def format_table_data(self, stats: np.ndarray, scale_info: ScaleInfo):
+    def format_table_data(self, stats: np.ndarray, scale_info: ScaleInfo, num_labels: int):
         scale_factor = scale_info.real_scale_length / scale_info.image_width if scale_info else 1
         scaled_areas = self.__get_pixel_areas(stats) * scale_factor
         scaled_diameters = self.__get_diameters(stats) * scale_factor
@@ -44,6 +57,7 @@ class SegmentationAnalyzer():
         diameter_std = np.std(scaled_diameters).round(2)
 
         table_data = {
+        "Count":    [num_labels, num_labels, num_labels, num_labels],  
         "Area":    [area_mean, area_min, area_max, area_std],  
         "Diameter":    [diameter_mean, diameter_min, diameter_max, diameter_std] 
         }
